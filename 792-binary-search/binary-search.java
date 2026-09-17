ebdee316251fb924efa94f50e387eacda0df95f4
Debastuti De <debastutide@gmail.com>
2026-09-17 19:52:35 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (13.96%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,26 +1,39 @@
 class Solution {
-    public int search(int[] arr, int target) {
-        int start=0;
-        int end=arr.length-1;
-        while(start>end)
-        {
-            return -1;
-        }
-        while(start<=end)
-        {
-            int mid=start+(end-start)/2;
-            if(target>arr[mid])
-            {
-                start=mid+1;
-            }
-            else if(target<arr[mid])
-            {
-               end=mid-1;
-            }
-            else{
-                return mid;
-            }
-        }
-        return -1;
+    public int search(int[] nums, int target) {
+        return helper(nums, target, 0, nums.length-1);
+    }
+    public int helper(int [] nums,int target,int lo,int hi){
+        if(lo>hi) return -1;
+        int mid=lo+(hi-lo)/2;
+        if(nums[mid]==target) return mid;
+        else if(nums[mid]>target) return helper(nums, target, lo, mid-1);
+        else return helper(nums, target, mid+1, hi);
     }
 }
+
+// class Solution {
+//     public int search(int[] arr, int target) {
+//         int start=0;
+//         int end=arr.length-1;
+//         while(start>end)
+//         {
+//             return -1;
+//         }
+//         while(start<=end)
+//         {
+//             int mid=start+(end-start)/2;
+//             if(target>arr[mid])
+//             {
+//                 start=mid+1;
+//             }
+//             else if(target<arr[mid])
+//             {
+//                end=mid-1;
+//             }
+//             else{
+//                 return mid;
+//             }
+//         }
+//         return -1;
+//     }
+// }
