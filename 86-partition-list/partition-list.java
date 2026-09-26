feca62e303df007a52eb195f92d6c9c2a911440f
Debastuti De <debastutide@gmail.com>
2026-09-26 14:02:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (49.32%) - LeetSync

--- 86-partition-list/partition-list.java
+++ 86-partition-list/partition-list.java
@@ -8,42 +8,33 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
     public ListNode partition(ListNode head, int x) {
+        if(head==null || head.next==null) return head;
+        ListNode d1=new ListNode(-1);
+        ListNode d2=new ListNode(-1);
+        ListNode t1=d1;
+        ListNode t2=d2;
+        ListNode t=head;
+        while(t!=null){
+            if(t.val<x){
+                t1.next=t;
+                t=t.next;
+                t1=t1.next;
 
-        if(head == null || head.next == null)
-            return head;
-
-        ListNode d1 = new ListNode(-1);
-        ListNode d2 = new ListNode(-1);
-
-        ListNode t1 = d1;
-        ListNode t2 = d2;
-
-        ListNode t = head;
-
-        while(t != null) {
-
-            if(t.val < x) {
-                t1.next = t;
-                t1 = t1.next;
             }
-            else {
-                t2.next = t;
-                t2 = t2.next;
+            else{
+                t2.next=t;
+                t=t.next;
+                t2=t2.next;
             }
-
-            t = t.next;
         }
+       
+        t2.next=null;
+        // d1=d1.next;
+        // d2=d2.next;
+        t1.next=d2.next;
 
-        // End the second list
-        t2.next = null;
-
-        // Connect both lists
-        t1.next = d2.next;
-
-        // Return actual head
         return d1.next;
     }
 }
